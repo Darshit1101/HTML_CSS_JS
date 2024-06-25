babe6630803132ccc22cfdb87bc91fe6864914f5
Darshit1101 <143336283+Darshit1101@users.noreply.github.com>
2024-06-25 11:19:20 +0530

    Initial commit of HTML, CSS, and JavaScript files

--- Website/script.js
+++ Website/script.js
@@ -11,17 +11,42 @@ const displayTabData = (tab) => {
     //add active class on tab
     document.getElementById('faq-tab-' + tab).classList.add('active')
 }
+
+
+// const displayTabDatatwo = (tab) => {
+//   // Get all elements with the class "tab-pan-new"
+//   let elements = document.querySelectorAll('.tab-pan-newtwo');
+
+
+//   // Loop through each element and remove the "active" class
+//   elements.forEach(element => {
+//       element.classList.remove('active');
+//   });
+
+//   //add active class on tab
+//   document.getElementById('faq-tabtwo-' + tab).classList.add('active')
+// }
+
+
+
+
 const displayTabDatatwo = (tab) => {
-  // Get all elements with the class "tab-pan-new"
-  let elements = document.querySelectorAll('.tab-pan-newtwo');
+    // Get all elements with the class "tab-pan-new"
+    let elements = document.querySelectorAll('.tab-pan-newtwo');
+  
+  
+    // Loop through each element and remove the "active" class
+    elements.forEach(element => {
+        element.classList.remove('active');
+    });
+  
+    //add active class on tab
+    document.getElementById('faq-tabtwo-' + tab).classList.add('active')
+  }
 
 
-  // Loop through each element and remove the "active" class
-  elements.forEach(element => {
-      element.classList.remove('active');
-  });
 
-  //add active class on tab
-  document.getElementById('faq-tabtwo-' + tab).classList.add('active')
-}
+
+
+
 
